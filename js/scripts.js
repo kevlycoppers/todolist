054ef1af8db09475713cbb23653f8dddcf3022a4
Levi Hopkins and Ryan Streur <levi+ryan@>
2016-06-13 14:24:40 -0700

    Define the ToDo object, and add functions to construct ToDo instances from user input. Add functions to allow users to display attributes of individual instances by clicking their names in a list

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,26 @@ function ToDo(name, due, description){
   this.description = description;
   globalToDoArray.push(this);
 }
+
+$(document).ready(function(){
+  $(".toDoForm").submit(function(event){
+    event.preventDefault();
+
+    var name = $("#nameInput").val();
+    var due = $("#dueDateInput").val();
+    var description = $("#descriptionInput").val();
+
+    var newToDo = new ToDo(name, due, description);
+    var index = globalToDoArray.length-1;
+    $(".toDoList").append("<li class='toDoItem item"+index+"'>"+newToDo.name+"</li>");
+
+    $(".toDoItem").click(function(event){
+      $(".descriptionItem").remove();
+      event.preventDefault();
+      var thisClass = $(this).attr("class")
+      var i = thisClass.charAt(thisClass.length-1);
+      console.log(i);
+      $(".detailList").append("<li class='descriptionItem'>Item: "+globalToDoArray[i].name+"</li><li class='descriptionItem'>Due: "+globalToDoArray[i].due+"</li><li class='descriptionItem'>Description: "+globalToDoArray[i].description+"</li>");
+    });
+  });
+});
